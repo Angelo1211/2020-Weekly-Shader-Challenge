e435caed65b610eced551fde6897d323aa2424c1
Angelo12 <angelo12@vt.edu>
2020-02-12 20:21:05 +0000

    Star field beginning

--- 07/test.glsl
+++ 07/test.glsl
@@ -1,5 +1,24 @@
 #include "./common.glsl"
 
+float
+makeStar(vec2 uv, float flare)
+{
+    float d = length(uv);
+    float star = .06/d;
+    float m = star;
+
+
+    float rays = max(1.0 - abs(uv.x*uv.y * 1000.0), 0.0);
+    m +=rays * flare;
+
+    uv = rotate(uv, M_PI/ 4.0);
+    rays = max(1.0 - abs(uv.x*uv.y * 1000.0), 0.0);
+    m +=rays*flare *0.3;
+
+    return m;
+}
+
+
 void
 mainImage(out vec4 fragColor, in vec2 fragPosition)
 {
@@ -7,14 +26,11 @@ mainImage(out vec4 fragColor, in vec2 fragPosition)
     uv *= 3.0;
 
     vec3 col = vec3(0.0);
-    float d = length(uv);
-    float star = .02/d;
-    col += star;
 
 
-    float rays = max(1.-abs(uv.x*uv.y * 10.0), 0.0);
-    col += rays;
-
+    vec2 gv = fract(uv) - 0.5;
+    col += makeStar(gv, 1.0);
+    //col.rg += gv;
 
 
     //col = pow(col, vec3(INV_GAMMA));
