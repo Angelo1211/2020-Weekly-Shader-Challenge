59de5f1f9afc92826746bb9869ffefd5b1f6285d
Angelo1211 <angelo12@vt.edu>
2020-02-29 00:44:22 +0000

    Added version with coloring!

--- 09/mandelbrot.glsl
+++ 09/mandelbrot.glsl
@@ -1,7 +1,11 @@
 #include "./common.glsl"
 
-#define CELLS 2.0
-#define AA 4
+/*
+    Note: Mouse selection is not working as expected in the visual studio code extension
+          but it is working fine in shadertoy. 
+*/
+#define AA 2
+
 
 vec2
 square(vec2 Z)
@@ -10,14 +14,14 @@ square(vec2 Z)
     return vec2(Z.x* Z.x - Z.y * Z.y, 2.0*Z.x*Z.y);
 }
 
-vec3
+float
 mandelbrot(vec2 uv, const int max_steps, float zoom, vec2 offset)
 {
-    vec3 col = vec3(0.0);
+    float res = -1.0;
 
-    vec2 C = uv;
+    vec2 C = uv + offset;
     C *= zoom;
-    C += offset;
+    
     vec2 Z  = vec2(0.0);
 
     for(int i = 0; i < max_steps; ++i)
@@ -26,21 +30,21 @@ mandelbrot(vec2 uv, const int max_steps, float zoom, vec2 offset)
 
         if(length(Z) > 2.0)
         {
-            col += vec3(1.0, 0.0, 1.0);
+            res = float(i);
             break;
         }
     }
 
-    return col;
+    return res;
 }
 
-vec3
+float
 julia(vec2 uv, const int max_steps, float zoom, vec2 offset)
 {
-    vec3 col = vec3(0.0);
+    float res = -1.0;
 
-    vec2 C = offset;
-    vec2 Z  = uv;
+    vec2 C = offset* zoom;
+    vec2 Z = uv;
 
     for(int i = 0; i < max_steps; ++i)
     {
@@ -49,21 +53,22 @@ julia(vec2 uv, const int max_steps, float zoom, vec2 offset)
 
         if(length(Z) > 2.0)
         {
-            col += vec3(1.0, 1.0, 1.0);
+            res = float(i);
             break;
         }
     }
 
-    return col;
+    return res;
 }
 
 void
 mainImage(out vec4 fragColor, in vec2 fragPos)
 {
+    float time = iTime / 10.0;
     float AR = iResolution.x / iResolution.y;
     vec3 col;
     vec3 tot = vec3(0.0);
-    vec2 mouse = (iMouse.xy - 0.5*iResolution.xy) / iResolution.x;
+    vec2 mouse = (iMouse.xy - 0.5*iResolution.xy) / iResolution.y;
     //mouse.y = 1.0 - mouse.y;
     //vec2 mouse = (iMouse.xy) / iResolution.xy;
 
@@ -83,31 +88,28 @@ mainImage(out vec4 fragColor, in vec2 fragPos)
         uv2 = vec2(1.0 - uv2.x, uv2.y);
         vec2 juv = (fract(uv2) - 0.5);
         juv.x *= AR;
+        
 
-        const int max_steps = 500;
-        float zoom = 2.0;
-        vec2 mov = vec2(0.0);
-        vec2 movement = mouse.xy;
+        const int max_steps = 50;
+        float zoom = 1.0;
+        vec2 mov = vec2(-1.5, 0.0);
+        vec2 movement = mov + mouse.xy;
 
         float inJulia = saturate(1.0 - id.y) * saturate(1.0 - id.x); 
         float inMandelbrot = 1.0 - inJulia;
-
-        col = mandelbrot(muv, max_steps, zoom, vec2(0.0) ) * inMandelbrot;
-        col += mandelbrot(juv, max_steps, zoom, vec2(0.0)) * inJulia;
-        col += (length(muv - mouse.xy) < 0.02) ? vec3(1.0) : vec3(0.0);
+        
+        float mI = mandelbrot(muv, max_steps, zoom, mov );
+        float jI = julia(juv, max_steps, zoom, movement);
+		
+        col += (mI<0.0) ? vec3(0.0) : 0.5 + 0.5*cos( pow(zoom,0.22)*mI*0.05 + vec3(7.0,2.0,3.0));
+        col *= inMandelbrot;
+        col += (jI<0.0) ? vec3(0.0) : (0.5 + 0.5*cos( pow(zoom,0.21)*jI*0.05 + vec3(2.0,2.0,5.0))) * inJulia;
+     
+        
+        col += (length(muv - mouse.xy) < 0.01) ? vec3(1.0) : vec3(0.0);
+        //col += (length(juv - mouse.xy) < 0.01) ? vec3(1.0) * inJulia : vec3(0.0) ;
         //col += vec3(mouse.xy, 0.0) * inJulia;
         //col += vec3(0.0, 1.0, 0.0) * inJulia;
-
-
-#if 0
-        float bound = 0.889;
-
-        bool mandelTopBound = muv.x > bound;
-        bool juliaTopBound = (juv.x) > bound;
-
-        col = ( (mandelTopBound) ? vec3(0.0, 0.0, 1.0) : vec3(0.0)) * inMandelbrot;
-        col += ( (juliaTopBound) ? vec3(1.0, 0.0, 0.0) : vec3(0.0)) * inJulia;
-#endif
 
         tot += col;
     }
