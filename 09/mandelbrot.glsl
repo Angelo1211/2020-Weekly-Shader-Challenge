94d3f8b00015bf18de4e4acb343c88273f442f1f
Angelo1211 <angelo12@vt.edu>
2020-03-01 16:25:07 +0000

    Updated mandelbrot shader with some zoom controls

--- 09/mandelbrot.glsl
+++ 09/mandelbrot.glsl
@@ -4,8 +4,27 @@
     Note: Mouse selection is not working as expected in the visual studio code extension
           but it is working fine in shadertoy. 
 */
-#define AA 2
 
+#define AA 2
+#define ZOOMTOGGLE 0
+#define OFFSETX -1.2
+#define OFFSETY 0.1
+
+/*
+	Shader Sundays! (9/52) 
+	"Mandelbrot&Julia Explorer"
+	
+	The inspiration for this shader came from watching this very cool video from numberphile
+	Where they explain the relationship between the Mandelbrot & Julia set, I wanted to see 
+	it for myself so here it is. There's some hacked in zoom I thought to put in last min,
+	because you can't have a mandelbrot set without zoom. The color gradient I got from the 
+	recent IQ shader. 
+	
+	Numberphile vid:
+	https://youtu.be/FFftmWSzgmk
+	IQ Mandelbrot Shader:
+	https://www.shadertoy.com/view/ttVSDW
+*/
 
 vec2
 square(vec2 Z)
@@ -18,10 +37,7 @@ float
 mandelbrot(vec2 uv, const int max_steps, float zoom, vec2 offset)
 {
     float res = -1.0;
-
-    vec2 C = uv + offset;
-    C *= zoom;
-    
+    vec2 C = uv*zoom + offset;
     vec2 Z  = vec2(0.0);
 
     for(int i = 0; i < max_steps; ++i)
@@ -42,13 +58,11 @@ float
 julia(vec2 uv, const int max_steps, float zoom, vec2 offset)
 {
     float res = -1.0;
-
-    vec2 C = offset* zoom;
+    vec2 C = offset;
     vec2 Z = uv;
 
     for(int i = 0; i < max_steps; ++i)
     {
-        //a^2 - b^2 + 2abi
         Z = square(Z) + C;
 
         if(length(Z) > 2.0)
@@ -64,52 +78,56 @@ julia(vec2 uv, const int max_steps, float zoom, vec2 offset)
 void
 mainImage(out vec4 fragColor, in vec2 fragPos)
 {
+    //Setup
     float time = iTime / 10.0;
     float AR = iResolution.x / iResolution.y;
-    vec3 col;
+    vec3 col = vec3(0.0);
     vec3 tot = vec3(0.0);
     vec2 mouse = (iMouse.xy - 0.5*iResolution.xy) / iResolution.y;
-    //mouse.y = 1.0 - mouse.y;
-    //vec2 mouse = (iMouse.xy) / iResolution.xy;
-
-    //AA for the extra niceness
+    
+    //SS AA loop
     for(int i = 0; i < AA; ++i)
     for(int j = 0; j < AA; ++j)
     {
         vec2 offset = vec2(i, j) / float(AA) - 0.5;
-
+		
         vec2 muv = ((fragPos + offset) - 0.5*iResolution.xy ) / iResolution.y;
         vec2 uv = ((fragPos + offset)) / iResolution.xy;
-
+		
+        //Minimap setup
         vec2 uv2 = uv;
         uv2.x = 1.0 - uv2.x;
         uv2 *= vec2(2.6, 2.6);
         vec2 id = floor(uv2);
         uv2 = vec2(1.0 - uv2.x, uv2.y);
         vec2 juv = (fract(uv2) - 0.5);
-        juv.x *= AR;
-        
-
-        const int max_steps = 50;
+        juv.x *= AR;     
+		float inJulia = saturate(1.0 - id.y) * saturate(1.0 - id.x); 
+        float inMandelbrot = 1.0 - inJulia;
+        		
+        //Zoom 
+        const int max_steps = 500;
         float zoom = 1.0;
-        vec2 mov = vec2(-1.5, 0.0);
+        #if ZOOMTOGGLE
+        zoom = (sin(time)* 0.5 + 0.5)*float(ZOOMTOGGLE);
+        #endif
+        
+        //Offset and Mouse movement
+        vec2 mov = vec2(OFFSETX, OFFSETY);
         vec2 movement = mov + mouse.xy;
 
-        float inJulia = saturate(1.0 - id.y) * saturate(1.0 - id.x); 
-        float inMandelbrot = 1.0 - inJulia;
-        
-        float mI = mandelbrot(muv, max_steps, zoom, mov );
+    	//Num Steps for Mandelbrot and Julia
+        float mI = mandelbrot(muv, max_steps, zoom, mov );        
         float jI = julia(juv, max_steps, zoom, movement);
 		
+        //Applying mandelbrot and julia colors
         col += (mI<0.0) ? vec3(0.0) : 0.5 + 0.5*cos( pow(zoom,0.22)*mI*0.05 + vec3(7.0,2.0,3.0));
         col *= inMandelbrot;
-        col += (jI<0.0) ? vec3(0.0) : (0.5 + 0.5*cos( pow(zoom,0.21)*jI*0.05 + vec3(2.0,2.0,5.0))) * inJulia;
-     
+        col += (jI<0.0) ? vec3(0.0) : (0.5 + 0.5*cos( pow(zoom,0.21)*jI*0.05 + vec3(2.0,2.0,5.0))) * inJulia;     
         
-        col += (length(muv - mouse.xy) < 0.01) ? vec3(1.0) : vec3(0.0);
-        //col += (length(juv - mouse.xy) < 0.01) ? vec3(1.0) * inJulia : vec3(0.0) ;
-        //col += vec3(mouse.xy, 0.0) * inJulia;
-        //col += vec3(0.0, 1.0, 0.0) * inJulia;
+        //Mouse markers
+        col += (length(muv - mouse.xy) < 0.005) ? vec3(1.0) : vec3(0.0);
+        col += (length(juv - mouse.xy) < 0.01) ? vec3(1.0) * inJulia : vec3(0.0) ;
 
         tot += col;
     }
