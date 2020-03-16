fda6b459992de494162c9ea3a8f704a6544ad745
Angelo1211 <angelo12@vt.edu>
2020-03-16 01:18:43 +0000

    Final tweaks to the saturn Shader!

--- 11/ALFP.glsl
+++ 11/ALFP.glsl
@@ -1,6 +1,9 @@
 #include "./common.glsl"
 
-vec3 _temp;
+float
+hash(vec2 n){
+    return fract(sin(dot(n,vec2(12.9898, 4.1414)))*43758.5453);
+}
 
 float
 sdRing(vec3 p)
@@ -8,12 +11,11 @@ sdRing(vec3 p)
     vec3 pos = p - vec3(-0., -0.00, 0.0);
 
     float r = length(vec2(pos.x, pos.z));
-    
-    //pos.xy = rotate(pos.xy, -0.2);
     float ringSize = 2.0;
     float d = sdBox(pos, vec3(ringSize, 0.00002, ringSize));
-
-    if(r > ringSize - 0.2 || r < 1.1) d += 0.01;
+    if(r > ringSize || r < 1.35) d += 0.001;
+    if(r > 1.75 && r < ringSize - 0.18)  d += 0.001;
+    if(r > 1.975 && r < ringSize - 0.02)  d += 0.001;
 
     return d;
 }
@@ -26,19 +28,18 @@ Map(vec3 p)
 {
     vec2 res = vec2(1e10, -1.0);
 
-    vec3 saturnPos = vec3(0., 0.00, 0.0);
-    UOP(sdSphere(p - saturnPos, 0.80), SATURN_ID);
+    UOP(sdSphere(p, 0.85), SATURN_ID);
+    UOP(sdRing(p), RING_ID);
 
-    //UOP(sdSphere(p - vec3(-0.013, -0.027, -0.9), 0.006), MOON_ID);
-
-    UOP(sdRing(p - saturnPos), RING_ID);
-
+    float ang = -iTime/ 12.0 + M_PI;
+    float rad = 1.5;
+    UOP(sdSphere(p - vec3(rad*sin(ang), 0.2, rad*cos(ang)), 0.05), MOON_ID);
 
     return res;
 }
 
 #define MAX_DIST 20.0
-#define MIN_DIST 0.001
+#define MIN_DIST 0.0001
 #define MAX_STEPS 200
 vec2
 RayMarch(vec3 ro, vec3 rd)
@@ -49,13 +50,11 @@ RayMarch(vec3 ro, vec3 rd)
     for(int i = 0; i < MAX_STEPS && t < MAX_DIST; ++i)
     {
         vec2 hit = Map(ro + t*rd);
-
         if(abs(hit.x) < t* MIN_DIST)
         {
             res = vec2(t, hit.y);
             break;
         }
-
         t+= hit.x;
     }
 
@@ -79,13 +78,20 @@ Material(float id, vec3 pos)
 
     if(id == SATURN_ID )
     {
-        col = vec3(0.875, 0.7, 0.097);
+        vec2 uv = vec2(atan(pos.x, pos.z) / (2.0*M_PI) + 0.5, pos.y *0.5 + 0.5);
+        col = vec3(0.789, 0.765, 0.43);
+        if(uv.y > 0.91) col *= 0.95;
     }
 
-    if(id == MOON_ID )
+    if(id == RING_ID )
     {
-        col = vec3(0.875, 0.7, 0.097) * 0.24;
+        float r = length(pos.xz);
+        r -= 1.0;
+        r = 1.0 - r;
+        col = mix(vec3(0.875, 0.7, 0.097) * 0.2, vec3(0.875, 0.7, 0.5)*1.0, r);
     }
+
+    if(id == MOON_ID) col = vec3(0.25, 0.6, 0.375);
 
     return col;
 }
@@ -93,35 +99,28 @@ Material(float id, vec3 pos)
 float
 CalcShadow(vec3 ro, vec3 rd)
 {
-    float k = 256.0;
-    float n = 1.0;
-
     for(float t = 0.10; t < MAX_DIST;)
     {
         float h = Map(ro + t*rd).x;
 
         if(h < MIN_DIST)
-        {
             return 0.0;
-        }
-
-        n = min(n, k*h/t);
-
         t += h;
     }
-    return n;
+    return 1.0;
 }
 
 vec3
-Render(vec3 ro, vec3 rd)
+Render(vec3 ro, vec3 rd, vec2 uv)
 {
     vec3 col = vec3(0.0);
-
     vec2 res = RayMarch(ro, rd);
     float t = res.x;
     float id = res.y;
 
     //Sky
+    col += vec3(1.0) * pow(hash(uv * vec2(13.0, 2.2)), 703.58);
+    col *= 6.0;
 
     //Opaque
     if(id >= 0.0)
@@ -132,13 +131,11 @@ Render(vec3 ro, vec3 rd)
 
         //Material
         col = Material(id, P);
-        //col = vec3(1.0);
 
         //Lighting
         vec3 lin = vec3(0.0);
-        //vec3 L = normalize(vec3(-0.7, 0.1, -0.3));
-        vec3 L = normalize(vec3(0.6, 0.5, 0.5));
-        //vec3 L = normalize(vec3(-0.6, 0.2, -0.1));
+        float ang = M_PI / 4.2 + iTime / 25.0 ;
+        vec3 L = normalize(vec3(cos(ang), 0.5, sin(ang)));
         float diff = saturate(dot(L, N));
         float amb = 0.05;
 
@@ -147,13 +144,11 @@ Render(vec3 ro, vec3 rd)
 
         //Shading
         float isRing =  (id == RING_ID) ? 0.0 : 1.0;
-        lin += 0.04 * amb  * vec3(1.0) * isRing;
-        lin += 1.0 * diff * vec3(1.0, 1.0, 1.0);
-        //lin += N;
+        isRing *= P.y * 6.0;
+        lin += 0.07 * amb  * vec3(1.0) * isRing;
+        lin += 1.2 * diff * vec3(1.0, 1.0, 1.0);
         col *= lin;
     }
-    //Volumetric
-    
     return saturate(col);
 }
 
@@ -162,7 +157,7 @@ void
 mainImage(out vec4 fragColor, in vec2 fragPos)
 {
     float nearP = 1.8;
-    float roll = 0.001;
+    float roll = 0.0;
     vec3 tot = vec3(0.0);
 
 #if AA > 1
@@ -175,20 +170,11 @@ mainImage(out vec4 fragColor, in vec2 fragPos)
         vec2 uv = ((fragPos) - 0.5*iResolution.xy)/iResolution.y;
 #endif
 
-#if 0
-        vec3 ta = vec3(0.0, -0.025, 0.0);
-        vec3 ro = ta + vec3(0.0, 0.0, -1.0);
-#else
-        //vec3 ta = vec3(0.0, -0.025, 0.0);
-        _temp = vec3(0.0, -0.00, 0.0);
-        vec3 ta = _temp;
-        vec3 ro = ta + vec3(0.0, 2.8, -4.0);
-#endif
+        vec3 ta = vec3(0.0, 0.00, 0.0);
+        vec3 ro = ta + vec3(0.0, 1.7, -4.5);
         mat3 cam = SetCamera(ro, ta, roll);
-
         vec3 rd = cam * normalize(vec3(uv, nearP));
-
-        vec3 col = Render(ro, rd);
+        vec3 col = Render(ro, rd, uv);
 
         GAMMA(col);
         tot += col;
