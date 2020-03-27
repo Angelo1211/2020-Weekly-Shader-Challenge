f4f7fb1c4071eb0eca4c8d8bdb5282df9dd8ed12
Angelo1211 <angelo12@vt.edu>
2020-03-27 22:41:31 +0000

    Reverting back to previous attempt

--- 12/naiveSkyScattering.glsl
+++ 12/naiveSkyScattering.glsl
@@ -1,90 +1,25 @@
 #include "./common.glsl"
-
 /*
     Reading this today:
     https://www.scratchapixel.com/lessons/procedural-generation-virtual-worlds/simulating-sky/simulating-colors-of-the-sky
 
-    Read the article for the first time, will implement tomorrow.
+    Restarting, working o
 */
 
-//Raymarcher vars
-#define MAX_STEPS 200
-#define MIN_DIST 0.0001
-#define MAX_DIST 2000.0
+//uniforms
+float time = 0.0;
 
-//Map vars
-#define EARTH_ID 0.0
-#define SPHERE_ID 1.0
-
-//Common vars
-vec3 sunDir = normalize(vec3(1.0, 1.0, 0.0));
-const float earthRadius = 6360e3; // (m)
-const float atmosphereRadius = 6420e3; // (m)
-float time;
-
-vec2
-Map(vec3 p)
-{
-    vec2 res = vec2(1e10, -1.0);
-    UOP(sdSphere(p - vec3(0.1, 0.1, 0.0), 0.25), SPHERE_ID);
-    return res;
-}
-
-vec2
-RayMarch(vec3 ro, vec3 rd)
-{
-    vec2 res = vec2(-1.0, -1.0);
-    float t = 0.0;
-
-    for(int i = 0; i < MAX_STEPS && t < MAX_DIST; ++i)
-    {
-        vec2 hit = Map(ro +t*rd);
-
-        if((hit.x) < MIN_DIST)
-        {
-            res = vec2(t, hit.y);
-            break;
-        }
-        t +=hit.x;
-    }
-    return res;
-}
+//const variables
+const float R_earth = 6360e3;
+const float R_atmo  = 6420e3;
+vec3 sun_direction = vec3(1.0, 0.0, 0.0);
 
 vec3
-Render(vec3 ro, vec3 rd)
+compute_incident_light(vec3 ro, vec3 rd)
 {
-    //Ray Results
     vec3 col = vec3(0.0);
 
-    vec2 res = RayMarch(ro, rd);
-    float t  = res.x;
-    float id = res.y;
-
-    //Solids
-    if(id >= 0.0)
-    {
-        //Geometry
-        vec3 P = ro + t*rd;
-
-        //Material
-        col = vec3(1.0);
-
-        //Lighting
-        vec3 lin = vec3(0.0);
-
-        //Shadowing
-
-        //Shading
-        lin += P;
-        col *= lin;
-    }
-    else //Sky
-    {
-        col = vec3(0.0);
-    }
-    //Volumetrics?
-
-    return (col);
+    return col;
 }
 
 void 
@@ -95,21 +30,22 @@ mainImage(out vec4 fragColor, in vec2 fragCoord)
     vec2 uv = ((fragCoord) / iResolution.xy)  - 0.5;
 
     //Init common vars
-    //sunDir = vec3(0.0, 1.0, 0.0);
-    time = iTime / 10.0;
     vec3 col = vec3(0.0);
+    time = iTime / 10.0;
+    vec2 p = nuv;
 
-    //We're standing a bit above the earth's surface looking about a meter ahead
-    vec3 ro = vec3(0.0, 0.0 , -1.0);
-    vec3 ta = vec3(0.0, 0.0, 0.0);
+    //Sky fish-eye representation
+    float z2 = length(p);
+    float phi = atan(p.y, p.x);
+    float theta = acos(1.0 - z2);
 
-    //Init camera vars
-    float nearp = 1.0;
-    float roll = 0.0;
-    mat3 cam = SetCamera(ro, ta, roll);
-    vec3 rd = cam * normalize(vec3(nuv, nearp));
+    vec3 rd = vec3(sin(theta) * cos(phi),
+                    cos(theta),
+                    sin(theta) * sin(phi));
 
-    col = Render(ro, rd);
+    vec3 ro = vec3(0, R_earth + 1.0, 0.0 );
+
+    col += compute_incident_light(ro, rd);
 
     GAMMA(col);
     fragColor = vec4(col, 1.0);
