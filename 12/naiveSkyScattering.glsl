468f0255b66985774ff9cbf4fe52064a063aaaad
Angelo1211 <angelo12@vt.edu>
2020-03-28 20:56:02 +0000

    check

--- 12/naiveSkyScattering.glsl
+++ 12/naiveSkyScattering.glsl
@@ -6,18 +6,27 @@
     Restarting, working o
 */
 
-//uniforms
-float time = 0.0;
-
 //const variables
 const float R_earth = 6360e3;
 const float R_atmo  = 6420e3;
-vec3 sun_direction = vec3(1.0, 0.0, 0.0);
+
+//uniforms
+float time = 0.0;
+
+vec2
+RayIntersectSphere(vec3 ro, vec3 rd, float r)
+{
+
+}
 
 vec3
-compute_incident_light(vec3 ro, vec3 rd)
+Render(vec3 ro, vec3 rd)
 {
-    vec3 col = vec3(0.0);
+    //Ray setup
+    float t0, t1;
+    vec3 col;
+
+    col += rd;
 
     return col;
 }
@@ -39,13 +48,14 @@ mainImage(out vec4 fragColor, in vec2 fragCoord)
     float phi = atan(p.y, p.x);
     float theta = acos(1.0 - z2);
 
+    //One meter above earth surface
+    vec3 ro = vec3(0, R_earth + 1.0, 0.0 );
+
     vec3 rd = vec3(sin(theta) * cos(phi),
                     cos(theta),
                     sin(theta) * sin(phi));
 
-    vec3 ro = vec3(0, R_earth + 1.0, 0.0 );
-
-    col += compute_incident_light(ro, rd);
+    col += Render(ro, rd);
 
     GAMMA(col);
     fragColor = vec4(col, 1.0);
